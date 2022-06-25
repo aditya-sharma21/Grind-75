1c4522a45ea6c548d4391fed294d824a6212f009
Aditya Sharma <99742149+aditya-sharma21@users.noreply.github.com>
2022-06-25 14:35:30 +0530

    Add files via upload

---
+++ Tree Preorder.cpp
@@ -0,0 +1,15 @@
+ class Solution {
+public:
+    void help(TreeNode* root, vector<int>& ans)
+    {
+        if(root==NULL)return;
+        ans.push_back(root->val);
+        help(root->left, ans);
+        help(root->right, ans);
+    }
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        help(root,ans);
+        return ans;
+    }
+};
