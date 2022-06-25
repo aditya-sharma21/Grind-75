c2e009aed7d6e4a596dbfe7c6331534a128556dd
Aditya Sharma <99742149+aditya-sharma21@users.noreply.github.com>
2022-06-25 14:37:45 +0530

    Add files via upload

---
+++ Tree Postorder.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void help(TreeNode* root, vector<int> &ans){
+        if(root==NULL) return;
+        help(root->left, ans);
+        help(root->right, ans);
+        ans.push_back(root->val);
+        
+       
+        
+        
+    }
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        help(root,ans);
+        return ans;
+    }
+        
+    
+}
