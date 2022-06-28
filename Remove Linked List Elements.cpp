bba59f74055d0dd22aae20f13a7948a98761822c
Aditya Sharma <99742149+aditya-sharma21@users.noreply.github.com>
2022-06-28 20:01:22 +0530

    Add files via upload

---
+++ Remove Linked List Elements.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        if(head==NULL){
+            return NULL;
+        }
+        
+        if(head->val == val){
+            return removeElements(head->next, val);
+        }
+        else{
+            head->next = removeElements(head->next, val);
+        }
+        return head;
+    }
+}
