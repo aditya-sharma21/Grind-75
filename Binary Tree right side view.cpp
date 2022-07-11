19e07f418bc5c4a19be55b250a6bf83215dcf550
Aditya Sharma <99742149+aditya-sharma21@users.noreply.github.com>
2022-07-11 21:26:50 +0530

    Binary Tree right side view

---
+++ Binary Tree right side view.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void helper(TreeNode* root, int level, vector<int> &res){
+        if(root == NULL) return;
+        if(res.size() == level) res.push_back(root->val);
+        helper(root->right, level+1, res);
+        helper(root->left, level+1, res);
+    }
+    
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> res;
+        helper(root, 0, res);
+        return res;
+    }
+};
