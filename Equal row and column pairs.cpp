2906c04000ef71939465028047415345b9626e1e
Aditya Sharma <99742149+aditya-sharma21@users.noreply.github.com>
2022-07-25 22:14:05 +0530

    Equal row and column pairs

---
+++ Equal row and column pairs.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int equalPairs(vector<vector<int>>& grid) 
+    {
+        int cnt = 0;
+        int n = grid.size();
+        
+        for(int i=0;i<n;i++)           
+        {
+            for(int j=0;j<n;j++)      
+            {
+                for(int k=0;k<n;k++)
+                {
+                    
+                    if(grid[i][k]!=grid[k][j])
+                        break;
+                    
+                   
+                    if(k==n-1)
+                        cnt++;
+                }
+            }   
+        }
+        
+        return cnt;
+    }
+};
