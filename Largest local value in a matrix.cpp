167e0ace8feae0e5e7883a4e54d5439fdfeafa43
Aditya Sharma <99742149+aditya-sharma21@users.noreply.github.com>
2022-08-14 21:30:49 +0530

    Largest local value in a matrix

---
+++ Largest local value in a matrix.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> largestLocal(vector<vector<int>>& grid) 
+    {
+        int n = grid.size();
+        vector<vector<int>> ans(n-2,vector<int> (n-2,0));
+        
+        
+        
+        for(int i=1;i<=n-2;i++)
+        {
+            for(int j=1;j<=n-2;j++)
+            {
+                int mx = INT_MIN;
+                mx = max(mx,max(grid[i-1][j-1],max(grid[i-1][j],grid[i-1][j+1])));   
+                mx = max(mx,max(grid[i][j-1],max(grid[i][j],grid[i][j+1])));          
+                mx = max(mx,max(grid[i+1][j-1],max(grid[i+1][j],grid[i+1][j+1])));  
+                
+                ans[i-1][j-1]=mx;
+            }
+        }
+        return ans;
+    }
+};
